00ed7482426f699c0f4325441c26e315f4346e44
FREDERIC MAQUET <frederic.maquet@ext.adeo.com>
2017-05-16 14:04:27 +0200

    Suppression repertoire outOfMemory_exemples

---
+++ infos_exemples/Main.java
@@ -0,0 +1,21 @@
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class Main {
+
+	private static void afficherVariables() {
+		Properties p = System.getProperties();
+		Enumeration<?> keys = p.keys();
+		while (keys.hasMoreElements()) {
+			String key = (String) keys.nextElement();
+			String value = (String) p.get(key);
+			System.out.println(key + ": " + value);
+		}
+
+	}
+
+	public static void main(String[] args) {
+		afficherVariables();
+	}
+}
+
