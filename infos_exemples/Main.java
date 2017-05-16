d786f458a1ee878edca9e152f35cce494649df3a
FREDERIC MAQUET <frederic.maquet@ext.adeo.com>
2017-05-16 14:07:40 +0200

    ajout methode lancerOutOfMemoryException()

--- infos_exemples/Main.java
+++ infos_exemples/Main.java
@@ -1,9 +1,18 @@
+
 import java.util.Enumeration;
 import java.util.Properties;
 
 public class Main {
 
-	private static void afficherVariables() {
+	public static final int SIZE = 2 * 1024 * 1024;
+
+	// pour lancer une OutOfMemoryException : java -Xmx12m Main
+	private static void lancerOutOfMemoryException() {
+		int[] i = new int[SIZE];
+	}
+
+
+	private static void afficherVariablesSysteme() {
 		Properties p = System.getProperties();
 		Enumeration<?> keys = p.keys();
 		while (keys.hasMoreElements()) {
@@ -15,7 +24,8 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		afficherVariables();
+		//lancerOutOfMemoryException()
+		afficherVariablesSysteme();
 	}
 }
 
